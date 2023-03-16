b185952976b82e0fcb759d37d086da9f1b350d47
Iheamarame Emmanuel Uchechukwu <princeucboy2017@gmail.com>
2023-03-16 14:03:49 -0700

    d

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - My first program that print a line with puts function
+ * Return: 0 (When it return Zero, it means Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
