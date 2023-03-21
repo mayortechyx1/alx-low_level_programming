3e9bd33b315ca2d921dec9c122d28acef0e852f4
Iheamarame Emmanuel Uchechukwu <princeucboy2017@gmail.com>
2023-03-21 11:28:29 -0700

    g

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,9 @@
 #include "main.h"
 
 /**
- * main - main code block
+ * main - write a program that prints _putchar
  *
- * Description: a program that prints _putchar, followed by a new line.
- *
- * Return: always 0 (success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
