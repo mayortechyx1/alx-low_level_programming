82c11abb8974f184bba76c987e3318e509975765
Iheamarame Emmanuel Uchechukwu <princeucboy2017@gmail.com>
2023-03-21 14:30:42 -0700

    d

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,8 @@
-#ifndef main_h
-#define main_h
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
 
-int _putchar(char);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -10,7 +11,7 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-int times_table(void);
+void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
